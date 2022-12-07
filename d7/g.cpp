54a2722098759fd4e801f183168bc8e48931e958
yahsieh <yunganh@uci.edu>
2022-12-07 00:21:26 -0800

    Add class DirPool and change structure

--- d7/g.cpp
+++ d7/g.cpp
@@ -19,55 +19,92 @@ class Dir {
         : parent(p), name(_s), isFile(b), fileSize(sz){};
 };
 
-int psize;
-vector<Dir> pool;
-int find(const Dir& cur, const string& s) {
-    for (const auto& i : cur.child) {
-        if (pool[i].name == s) return i;
-    }
-    return -1;
-}
+class DirPool {
+   private:
+    size_t psize;
+    vector<Dir> data;
 
-void add(Dir& cur, const int& curIdx, const string& name, bool isFile = false,
-         int sz = 0) {
-    if (psize >= POOL_SIZE) {
-        cerr << "reached pool limit=" << POOL_SIZE << endl;
-        exit(-1);
+   public:
+    DirPool(int n = 1) {
+        psize = 1;
+        data.resize(n);
     }
-    cur.child.push_back(psize);
-    pool[psize] = Dir{curIdx, name, isFile, sz};
-    ++psize;
-}
+    int find(const int& idx, const string& s) {
+        const auto& cur = data[idx];
+        for (const auto& i : cur.child) {
+            if (data[i].name == s) return i;
+        }
+        return -1;
+    }
+    void add(const int& idx, const string& name, bool isFile = false,
+             int sz = 0) {
+        auto& cur = data[idx];
+        if (psize >= data.size()) data.push_back(Dir{});
+        cur.child.push_back(psize);
+        data[psize] = Dir{idx, name, isFile, sz};
+        ++psize;
+    }
+    int dirSize(const int pIndex) {
+        auto& cur = get(pIndex);
+        if (cur.fileSize == 0) {
+            int fileSum = 0;
+            for (const auto& i : cur.child) {
+                const auto& next = get(i);
+                if (next.isFile) {
+                    DEBUG(cerr << next.name << " : " << next.fileSize << endl)
+                    fileSum += next.fileSize;
+                } else {
+                    const auto& fs = dirSize(i);
+                    DEBUG(cerr << next.name << "/ : " << fs << endl)
+                    fileSum += fs;
+                }
+            }
+            cur.fileSize = fileSum;
+        }
+        return cur.fileSize;
+    }
+    vector<int> allDirSize() {
+        vector<int> ret;
+        dirSize(0);
+        for (size_t i = 0; i < psize; i++) {
+            if (!data[i].isFile) ret.push_back(data[i].fileSize);
+        }
+        return ret;
+    }
 
-void parse() {
-    psize = 1;
+    Dir& get(const int& idx) { return data[idx]; }
+    const Dir& get(const int& idx) const { return data[idx]; }
+};
+
+DirPool parse() {
+    DirPool pool(POOL_SIZE);
     vector<string> v;
     string s;
     while (getline(cin, s)) v.push_back(s);
-    pool.resize(POOL_SIZE);
-    int cur = 0;
-    int index = 0;
+    size_t pIndex = 0;
+    size_t index = 0;
     while (index < v.size()) {
         if (v[index][0] == '$') {
             if (v[index][2] == 'c') {
                 if (v[index][5] == '/' && v[index].size() == 6) {
-                    cur = 0;
+                    pIndex = 0;
                 } else {
+                    auto cur = pool.get(pIndex);
                     if (v[index][5] == '.' && v[index][6] == '.') {
-                        cur = pool[cur].parent;
+                        pIndex = cur.parent;
                     } else {
                         auto dirName = v[index].substr(5);
-                        auto f = find(pool[cur], dirName);
+                        auto f = pool.find(pIndex, dirName);
                         if (f == -1) {
                             cerr << "error: cd to " << dirName
-                                 << " not found in " << pool[cur].name << endl;
-                            for (const auto& i : pool[cur].child) {
-                                if (!pool[i].isFile)
-                                    cerr << pool[i].name << endl;
+                                 << " not found in " << cur.name << endl;
+                            for (const auto& i : cur.child) {
+                                const auto& next = pool.get(i);
+                                if (!next.isFile) cerr << next.name << endl;
                             }
                             exit(-1);
                         } else {
-                            cur = f;
+                            pIndex = f;
                         }
                     }
                 }
@@ -77,29 +114,32 @@ void parse() {
                 while (index < v.size() && v[index][0] != '$') {
                     if (v[index][0] == 'd') {
                         auto dirName = v[index].substr(4);
-                        add(pool[cur], cur, dirName);
+                        pool.add(pIndex, dirName);
                     } else {
                         auto whitespace = v[index].find(' ');
                         auto sz = stoi(v[index].substr(0, whitespace));
                         auto fname = v[index].substr(whitespace + 1);
-                        add(pool[cur], cur, fname, true, sz);
+                        pool.add(pIndex, fname, true, sz);
                     }
                     ++index;
                 }
             }
         }
     }
+    return pool;
 }
 
-pair<int, int> calc(const Dir& cur, const int k) {
+pair<int, int> calc(const DirPool& pool, const int pIndex, const int k) {
     int fileSum = 0, dirSum = 0;
+    const auto& cur = pool.get(pIndex);
     for (const auto& i : cur.child) {
-        if (pool[i].isFile) {
-            DEBUG(cerr << pool[i].name << " : " << pool[i].fileSize << endl)
-            fileSum += pool[i].fileSize;
+        auto& next = pool.get(i);
+        if (next.isFile) {
+            DEBUG(cerr << next.name << " : " << next.fileSize << endl)
+            fileSum += next.fileSize;
         } else {
-            const auto& [fs, ds] = calc(pool[i], k);
-            DEBUG(cerr << pool[i].name << "/ : " << fs << endl)
+            const auto& [fs, ds] = calc(pool, i, k);
+            DEBUG(cerr << next.name << "/ : " << fs << endl)
             fileSum += fs;
             dirSum += ds;
             if (fs <= k) dirSum += fs;
@@ -107,42 +147,21 @@ pair<int, int> calc(const Dir& cur, const int k) {
     }
     return {fileSum, dirSum};
 }
+int q1(const DirPool& pool, int k) { return calc(pool, 0, k).second; }
 
-int calc(Dir& cur) {
-    if (cur.fileSize == 0) {
-        int fileSum = 0;
-        for (const auto& i : cur.child) {
-            if (pool[i].isFile) {
-                DEBUG(cerr << pool[i].name << " : " << pool[i].fileSize << endl)
-                fileSum += pool[i].fileSize;
-            } else {
-                const auto& fs = calc(pool[i]);
-                DEBUG(cerr << pool[i].name << "/ : " << fs << endl)
-                fileSum += fs;
-            }
-        }
-        cur.fileSize = fileSum;
-    }
-    return cur.fileSize;
-}
-
-int q1(int k) { return calc(pool[0], k).second; }
-
-int q2() {
-    set<int> st;
-    st.insert(0);
-    int64_t used = calc(pool[0]);
+int q2(DirPool& pool) {
+    int64_t used = pool.dirSize(0);
     int64_t need = 30000000 - (70000000 - used);
     DEBUG(cerr << "used = " << used << ", need to free = " << need << endl)
-    for (int i = 0; i < psize; i++) {
-        if (!pool[i].isFile) st.insert(pool[i].fileSize);
-    }
+    vector<int> dirs = pool.allDirSize();
+    set<int> st(dirs.begin(), dirs.end());
+    st.insert(0);
     return *st.lower_bound(need);
 }
 
 int main() {
-    parse();
-    cout << q1(100000) << endl;
-    cout << q2() << endl;
+    auto pool = parse();
+    cout << q1(pool, 100000) << endl;
+    cout << q2(pool) << endl;
     return 0;
 }
